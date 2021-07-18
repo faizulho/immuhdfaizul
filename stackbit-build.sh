4a22bd8f519060bfd63e10e3a46413ba869afeb5
Muhammad Faizullah Syarizzat <muhdfaizul@mail.ru>
2021-07-19 01:43:05 +0800

    Update stackbit-build.sh

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -10,6 +10,6 @@ echo "stackbit-build.sh: start build"
 npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/60d519b2a9eb24001674f5e1
 
 # build site
-hugo
+hugo version
 
 echo "stackbit-build.sh: finished build"
