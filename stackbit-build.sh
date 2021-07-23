88bd71d1508f915dd435565506994276a79c3cde
Muhammad Faizullah Syarizzat <muhdfaizul@mail.ru>
2021-07-23 22:31:09 +0800

    Update stackbit-build.sh

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -7,9 +7,10 @@ set -v
 echo "stackbit-build.sh: start build"
 
 # fetch data from CMS through stackbit-pull
+export STACKBIT_API_KEY={4a8b11f6bfdd43eaa69a402205cd09ea2dbaa5dd689f293a94e6ce7ab9cc8922}
 npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/60d519b2a9eb24001674f5e1
 
 # build site
-hugo
+hugo version
 
 echo "stackbit-build.sh: finished build"
